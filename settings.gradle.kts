23f2eea9c511d5fe2f88540256b92780a55ac7b0
flashgnash <flashgnash@proton.me>
2024-12-13 15:35:59 +0000

    Previous failed attempt

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+
+rootProject.name = "cr0s.warpdrive"
+
+pluginManagement {
+  repositories {
+    maven {
+      // RetroFuturaGradle
+      name = "GTNH Maven"
+      url = uri("https://nexus.gtnewhorizons.com/repository/public/")
+      mavenContent {
+        includeGroupByRegex("com\\.gtnewhorizons\\..+")
+        includeGroup("com.gtnewhorizons")
+      }
+    }
+    gradlePluginPortal()
+    mavenCentral()
+    mavenLocal()
+  }
+}
+
+plugins {
+  // Automatic toolchain provisioning
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
